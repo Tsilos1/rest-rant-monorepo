4c859d297bb987e9177cca35326badb71e4dd39c
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2023-01-08 14:32:59 -0600

    finished project

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
+    "username": "postgres",
+    "password": "Ufz8vsew",
+    "database": "rest-rant-monorepo",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
+    "username": "postgres",
+    "password": "Ufz8vsew",
+    "database": "rest-rant-monorepo-test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
+    "username": "postgres",
+    "password": "Ufz8vsew",
+    "database": "rest-rant-monorepo-production",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   }
 }
